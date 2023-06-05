010d7bf4ffcae37136d38ef5864154c0d09d71a1
Balakrishnan Govindasamy <Balakrishnan.Govindasamy@gisbizdev.gisbiz.chennai.com>
2023-06-05 16:17:07 +0530

    eureka-server

---
+++ src/test/java/com/tekb/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.tekb;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurekaServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
